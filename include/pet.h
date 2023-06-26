e4620b3411a6929368e8500e4891e311be7ad78f
An-Man <anna.pikkarainen@gmail.com>
2023-06-26 13:26:44 +0300

    add feature issue#1: create a new pet

--- include/pet.h
+++ include/pet.h
@@ -2,6 +2,9 @@
 #define PET_H
 
 #include <string>
+#include <vector>
+
+
 
 struct Pet {
     std::string name {};
@@ -11,5 +14,14 @@ struct Pet {
     int energy {};
     bool is_sleeping {false};
 };
+
+std::string input_pet_name();
+
+std::string input_pet_type();
+
+Pet create_new_pet();
+
+std::vector<Pet> add_pet_to_vector(std::vector<Pet>& pets, Pet new_pet);
+
 
 #endif
